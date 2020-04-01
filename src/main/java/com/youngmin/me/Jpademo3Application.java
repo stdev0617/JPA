98cf14ae02ded330581964440ad3fce213570144
youngmin <youngmin@ex-em.com>
2020-04-01 23:52:17 +0900

    first commit

---
+++ src/main/java/com/youngmin/me/Jpademo3Application.java
@@ -0,0 +1,13 @@
+package com.youngmin.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpademo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jpademo3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/youngmin/me/Jpademo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youngmin.me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpademo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
