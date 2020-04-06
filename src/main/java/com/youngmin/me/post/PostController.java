257845b02b9b358926b5e80d390fb943a9997696
stdev0617 <stdev0617@gmail.com>
2020-04-06 23:37:16 +0900

    introduction to spring data web function

---
+++ src/main/java/com/youngmin/me/post/PostController.java
@@ -0,0 +1,30 @@
+package com.youngmin.me.post;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class PostController {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @GetMapping("/posts/{id}")
+    public String getAPost(@PathVariable("id") Post post) {
+        return post.getTitle();
+    }
+
+    @GetMapping("/posts")
+    public PagedModel<EntityModel<Post>> getPosts(Pageable pageable, PagedResourcesAssembler<Post> assembler) {
+        Page<Post> all = postRepository.findAll(pageable);
+        return assembler.toModel(all);
+    }
+}
